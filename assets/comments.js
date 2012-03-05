6ccad18d8be735d73b0bd96c47eb8d4ecae07a8b
Dan F-M <danfm@nyu.edu>
2012-03-05 00:36:18 -0500

    adding the comments back

---
+++ assets/comments.js
@@ -0,0 +1,33 @@
+var github_url = "https://github.com/dfm/dfm.github.com/edit/master/_posts/";
+
+$(function() {
+    var obj = $("#comments").next();
+    var ul = $("<ul>");
+    var ncomments = 0;
+    var items = obj.find("li").each(function(i, obj) {
+        var groups = /(.*?) \- (.*?) \- (.*)/i.exec($(obj).html());
+        if ($.trim(groups[1]) != "github-username") {
+            var s = "<span class=\"comment-date\">"+groups[2]+"</span> ";
+            s += "<span class=\"comment-body\"><a href=\"https://github.com/"+groups[1]+"\">"+groups[1]+"</a> said:";
+            s += "<p class=\"comment\">"+groups[3]+"</p></span>";
+            ul.append($("<li>").html(s));
+            ncomments += 1;
+        }
+    });
+    obj.remove();
+    $("#comments").remove();
+    if (ncomments > 0) {
+        $("#comment-placeholder").append(ul);
+    } else {
+        $("#comments-section .section-title").hide();
+    }
+
+    $("#comment-btn").click(function () {
+        var data = $(this).data();
+        var path = data.title.split("/");
+        var url = github_url + data.date + "-" + path[path.length-1] + ".markdown";
+        window.location = url;
+        return false;
+    });
+});
+
