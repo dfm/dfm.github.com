53d56bfd63a833e3c012d372d5435626c46474d3
dfm <dfm@users.noreply.github.com>
2022-06-06 15:43:44 +0000

    deploy: dfm-io/dfm.io@1da6548c33aeb565cb5c5deb4113130d5da1526e

--- js/index.min.49e4d8a384357d9b445b87371863419937ede9fa77737522ffb633073aebfa44.js
+++
@@ -1,3 +0,0 @@
-function toggleMobileNavState(){const body=document.querySelector("body");body.classList.toggle("nav--active");}
-function initBurger(){const burger=document.querySelector(".burger");burger.addEventListener("click",toggleMobileNavState);}
-initBurger();
